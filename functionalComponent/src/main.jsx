097e4676dc2ff05c65cc5bfddeeaf84bfe071ed9
Awais Ahmad <awais6412l@gmail.com>
2022-12-03 13:54:11 +0500

    create project with vite

---
+++ functionalComponent/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Functionalcomponent from './components/Functionalcomponent'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Functionalcomponent />
+  </React.StrictMode>
+)
