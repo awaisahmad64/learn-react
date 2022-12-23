20c232aa524fb006d1f82d3c63e08f7b1994143e
Awais Ahmad <awais6412l@gmail.com>
2022-12-23 12:45:37 +0500

    create a problem unnecessary fun call

--- propdrillinginreact/src/components/ChildA.js
+++ propdrillinginreact/src/components/ChildA.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ChlidB from './ChlidB';
 
-function childA() {
+function ChildA() {
+  const [add, setAdd] = useState(0);
+  const [minus, setMinus] = useState(100);
+  const multipulication = () => {
+    console.log('mul run');
+    return add * 4;
+  };
   return (
     <div>
       <ChlidB />
+      <h1>Learm useMemo</h1>
+      {multipulication()}
+      <br />
+      <button
+        onClick={() => {
+          setAdd(add + 1);
+        }}
+      >
+        Add
+      </button>
+      <p>{add}</p>
+      <button
+        onClick={() => {
+          setMinus(minus - 1);
+        }}
+      >
+        Minus
+      </button>
+      <p>{minus}</p>
     </div>
   );
 }
 
-export default childA;
+export default ChildA;
