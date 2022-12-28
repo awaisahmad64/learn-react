3fddf59c9bcc58630f0c886ad7b2676a31cd2303
Awais Ahmad <awais6412l@gmail.com>
2022-12-28 21:10:26 +0500

    fix decrement lowercase bug

--- react-form/src/components/ReducerHook.jsx
+++ react-form/src/components/ReducerHook.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useReducer } from 'react';
+//hardcode value for const variable
 const ACTIONS = {
   INCREMENT: 'increment',
   DECREMENT: 'decrement',
@@ -9,7 +10,7 @@ function reducer(state, action) {
   switch (action.type) {
     case ACTIONS.INCREMENT:
       return { count: state.count + 1 };
-    case ACTIONS.decrement:
+    case ACTIONS.DECREMENT:
       return { count: state.count - 1 };
     default:
       return state;
@@ -29,7 +30,7 @@ function ReducerHook() {
       <center>
         <h1>Reducer Hook Example Here</h1>
         <button onClick={decrement}> -</button>
-        <span>{state.count >= 0 ? state.count : state.count=0}</span>
+        <span>{state.count >= 0 ? state.count : (state.count = 0)}</span>
         <button onClick={increment}> +</button>
       </center>
     </div>
