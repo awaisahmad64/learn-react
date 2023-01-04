9f55a54848507e8a7fa8457c9809377538f7205e
Awais Ahmad <awais6412l@gmail.com>
2023-01-04 09:46:06 +0500

    fix re-render issue by using useRef

--- stateinreact/src/components/UseRef.jsx
+++ stateinreact/src/components/UseRef.jsx
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef } from 'react';
 
 function UseRef() {
   const [name, setName] = useState('');
-  const [counter, setCounter] = useState(0);
+  const counterObject = useRef(0);
   useEffect(() => {
-    setCounter((prev) => prev + 1);
-  });
+    // setCounter((prev) => prev + 1);
+    counterObject.current = counterObject.current +1;
+  })
   return (
     <div>
       <form>
         <input onChange={(e) => setName(e.target.value)} />
         <p>Name:{name}</p>
-        <p>Counter : {counter}</p>
+        <p>Counter : {counterObject.current}</p>
       </form>
     </div>
   );
