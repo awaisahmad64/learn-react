55e987e4959592d8e4664e5fcbec60ce1bff624c
Awais Ahmad <awais6412l@gmail.com>
2023-01-28 12:56:46 +0500

    conditional styling using classnames pkg

--- className-using-pks-classnames/src/App.jsx
+++ className-using-pks-classnames/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import classNames from 'classnames';
 import './App.css';
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,9 +13,13 @@ function App() {
       </h1>
       <div className="box">
         <p className="title text-center" onClick={toggle}>
-          Click Me!
+          Click Me! <span className={classNames('iconPosition',{'plusNotShow':isOpen})}><b>+</b></span><span className={classNames('iconPositionMinus',{'minusNotShow':!isOpen})}><b>-</b></span>
         </p>
-        <div className={`description text-center ${isOpen? 'description-active':''}`}>
+        <div
+          className={classNames('description', {
+            'description-active': isOpen,
+          })}
+        >
           The total number of items returned by the hook should remain constant.
           This will avoid resizing our pagination component if the length of the
           range array changes while the user is interacting with the component.
