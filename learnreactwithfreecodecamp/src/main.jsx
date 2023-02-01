8e72f2160b38f237ff7351e8294525bfb04c4e26
Awais Ahmad <awais6412l@gmail.com>
2023-02-01 11:06:41 +0500

    render html element to the dom

--- learnreactwithfreecodecamp/src/main.jsx
+++ learnreactwithfreecodecamp/src/main.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Commentinjsx from './jsx/Commentinjsx';
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Commentinjsx />
-  </React.StrictMode>
+{
+  /* Render html element to the DOM */
+}
+const JSX = (
+  <div id="root">
+    <h1>Hello World</h1>
+    <p>Lets render this to the DOM</p>
+  </div>
 );
+ReactDOM.createRoot(document.getElementById('root')).render(JSX);
