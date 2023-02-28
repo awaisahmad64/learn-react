f87294efbcbe075d8b646efd6d5296fee2decd09
Awais Ahmad <awais6412l@gmail.com>
2023-02-28 21:15:22 +0500

    add sass variable

---
+++ helloWorld/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './app.scss';
+function App() {
+  return (
+    <div>
+      <h1>Hello, Awais </h1>
+    </div>
+  );
+}
+
+export default App;
--- helloWorld/src/main.jsx
+++ helloWorld/src/main.jsx
@@ -1,6 +1,5 @@
-import react from 'react'
-import ReactDOM from 'react-dom'
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <h1>Hello World!</h1>
-)
+import react from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
